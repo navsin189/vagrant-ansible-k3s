af8f6ea5a5cca8fef0fa08e2aff152f6a6686a11
unknown <navsin189@gmail.com>
2022-09-03 03:39:05 +0530

    installed docker to create my own image containing WebApp

---
+++ centos_docker/main.py
@@ -0,0 +1,58 @@
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, Request, Form
+import psycopg2,os
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+DATABASE = os.environ["DATABASE"]
+USER = os.environ["USER"]
+PASSWORD = os.environ["PASSWORD"]
+HOST = os.environ["HOST"]
+PORT = os.environ["PORT"]
+
+@app.get("/")
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html",{"request": request})
+
+@app.post("/register/")
+async def signup(request: Request,username: str = Form(),password: str = Form(),email: str = Form()):
+    try:
+        connection = psycopg2.connect(database=DATABASE, user = USER, password = PASSWORD, host = HOST, port = PORT)
+        cursor = connection.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS client(NAME TEXT NOT NULL,PASSWORD TEXT NOT NULL,EMAIL TEXT NOT NULL UNIQUE);''')
+        cursor.execute("SELECT EXISTS(SELECT 1 from client WHERE email='{email}' );".format(email=email))
+        user_exists = cursor.fetchone()[0]
+        #connection.commit()
+        if user_exists:
+            return templates.TemplateResponse("index.html",{"request": request,"register_msg":"User already exist, Please login"})
+        else:
+            cursor.execute("""INSERT INTO client(NAME,EMAIL,PASSWORD) VALUES ('{username}','{email}','{passwd}')""".format(username=username,email=email,passwd=password))
+        connection.commit()
+        connection.close()
+        return templates.TemplateResponse("index.html",{"request": request,"msg":"Successfully registered, please login"})
+    except:
+        return {"created" : "unSuccessful"}
+
+@app.post("/login/")
+async def signup(request: Request,password: str = Form(),email: str = Form()):
+    try:
+        connection = psycopg2.connect(database=DATABASE, user = USER, password = PASSWORD, host = HOST, port = PORT)
+        cursor = connection.cursor()
+        cursor.execute("SELECT EXISTS(SELECT 1 from client WHERE email='{email}' );".format(email=email))
+        user_exists = cursor.fetchone()[0]
+        cursor.execute("SELECT EXISTS(SELECT 1 from client WHERE email='{email}' AND password='{passwd}');".format(email=email,passwd=password))
+        user_exists_with_correct_password = cursor.fetchone()[0]
+        #connection.close()
+        if  user_exists  and user_exists_with_correct_password : # user exists
+            cursor.execute("SELECT name from client WHERE email='{email}';".format(email=email))
+            username = cursor.fetchone()[0]
+            return templates.TemplateResponse("dashboard.html",{"request": request,"username": username,"email": email})
+        elif user_exists_with_correct_password != user_exists: # incorrect password
+            return templates.TemplateResponse("index.html",{"request": request,"msg":"Incorrect Password"})
+        else: # no user exists
+            return templates.TemplateResponse("error.html",{"request": request,"status_code":"403","msg":"User not exist"})
+
+    except:
+            return templates.TemplateResponse("error.html",{"request": request,"status_code":"500","msg":"Sorry for the Inconvinence, there is some problem from our side"})
